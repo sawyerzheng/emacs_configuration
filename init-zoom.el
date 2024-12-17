376f0de5b8dc7b597450a69e13e8fd3c4b39cf49
Sawyer Zheng <kmxsz@qq.com>
2024-12-17 15:46:51 +0800

    fix:init-zoom.el: mode-line background color after load new theme

--- init-zoom.el
+++ init-zoom.el
@@ -12,6 +12,14 @@
 (require 'zoom-window)
 (custom-set-variables
  '(zoom-window-use-persp t))
+
+
 (zoom-window-setup)
+(setq zoom-window-mode-line-color "black")
+(defun my/zoom-window-after-theme-load-advice (&rest args)
+  (setq zoom-window--orig-color (face-background 'mode-line))
+  )
+
+(advice-add 'load-theme :after #'my/zoom-window-after-theme-load-advice)
 
 (global-set-key (kbd "C-x C-z") 'zoom-window-zoom)
