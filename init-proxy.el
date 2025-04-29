fc40f4f9ba2011a8cdb9c21630116cd4c1ac5546
Sawyer Zheng <kmxsz@qq.com>
2025-04-29 15:07:09 +0800

    Add LAN proxy support for EAF
    Added new function `enable-proxy-eaf-lan` to configure proxy settings
    for LAN environment in EAF.  The function sets proxy host to
    lan with http protocol and port 7890, similar to existing
    proxy functions.  Also includes restart reminder message for EAF
    process.

--- init-proxy.el
+++ init-proxy.el
@@ -107,6 +107,14 @@
         eaf-proxy-port "7890")
   (message "env http_proxy is %s now" (concat eaf-proxy-type "://" eaf-proxy-host ":" eaf-proxy-port)))
 
+(defun enable-proxy-eaf-lan ()
+  "remember to restart eaf using: `eaf-restart-process'."
+  (interactive)
+  (setq eaf-proxy-host "172.16.10.86"
+        eaf-proxy-type "http"
+        eaf-proxy-port "7890")
+  (message "env http_proxy is %s now" (concat eaf-proxy-type "://" eaf-proxy-host ":" eaf-proxy-port)))
+
 (defun enable-proxy-windows-eaf ()
   "remember to restart eaf using: `eaf-restart-process'."
   (interactive)
