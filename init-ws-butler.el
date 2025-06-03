e9299f3cd935a5d25a2b40ad511a458eabcc5123
Sawyer Zheng <kmxsz@qq.com>
2025-06-04 00:05:17 +0800

    change ws-butler use straight.el default recipe

--- init-ws-butler.el
+++ init-ws-butler.el
@@ -1,4 +1,4 @@
-(my/straight-if-use 'ws-butler)
+(my/straight-if-use '(ws-butler :type git :host github :repo "lewang/ws-butler")) 
 (use-package ws-butler
   :hook ((prog-mode org-mode markdown-mode gfm-mode) . ws-butler-mode))
 
