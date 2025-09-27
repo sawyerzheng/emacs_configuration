8479683ba2ca29b26bfe025d7c49036d430748c3
Sawyer Zheng <kmxsz@qq.com>
2025-09-27 21:43:32 +0800

    Fix: Align indent-bars configuration
    Correct the indentation of `indent-bars` settings
    and add a keybinding for `indent-bars-mode`.

--- init-indent-bars.el
+++ init-indent-bars.el
@@ -1,9 +1,8 @@
 ;; -*- coding: utf-8; -*-
-(my/straight-if-use '(indent-bars :type git :host github :repo "jdtsmith/indent-bars"))
 (use-package indent-bars
   ;; :hook ((python-mode yaml-mode) . indent-bars-mode)
   :hook (prog-mode . indent-bars-mode)
-
+  :bind ("C-c u i" . indent-bars-mode)
   :config
   (require 'indent-bars-ts) 		; not needed with straight
   (setq indent-bars-width-frac 0.1)
@@ -18,20 +17,20 @@
   ;;  indent-bars-display-on-blank-lines nil)
 
   (setq
-    indent-bars-color '(highlight :face-bg t :blend 0.15)
-    indent-bars-pattern "."
-    indent-bars-width-frac 0.13
-    indent-bars-pad-frac 0.1
-    indent-bars-zigzag nil
-    indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1) ; blend=1: blend with BG only
-    indent-bars-highlight-current-depth '(:blend 0.5) ; pump up the BG blend on current
-    indent-bars-display-on-blank-lines t)
+   indent-bars-color '(highlight :face-bg t :blend 0.15)
+   indent-bars-pattern "."
+   indent-bars-width-frac 0.13
+   indent-bars-pad-frac 0.1
+   indent-bars-zigzag nil
+   indent-bars-color-by-depth '(:regexp "outline-\\([0-9]+\\)" :blend 1) ; blend=1: blend with BG only
+   indent-bars-highlight-current-depth '(:blend 0.5) ; pump up the BG blend on current
+   indent-bars-display-on-blank-lines t)
   :custom
   (indent-bars-treesit-support t)
   (indent-bars-treesit-ignore-blank-lines-types '("module"))
   ;; Add other languages as needed
   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
-	                               if_statement with_statement while_statement)))
+	                        if_statement with_statement while_statement)))
   ;; wrap may not be needed if no-descend-list is enough
   ;;(indent-bars-treesit-wrap '((python argument_list parameters ; for python, as an example
   ;;				      list list_comprehension
