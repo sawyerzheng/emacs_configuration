736fd82f46bd38451b09db1d8adc95a455b783c7
Sawyer Zheng <kmxsz@qq.com>
2025-09-27 21:47:13 +0800

    feat(vertigo): Add vertico-directory for enhanced directory navigation
    Configure vertico-directory to improve directory
    navigation commands and tidy shadowed file names.

--- init-vertico.el
+++ init-vertico.el
@@ -45,6 +45,18 @@
 		 args)))
   )
 
+;; Configure directory extension.
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  ;; More convenient directory navigation commands
+  :bind (:map vertico-map
+              ("RET" . vertico-directory-enter)
+              ("DEL" . vertico-directory-delete-char)
+              ("M-DEL" . vertico-directory-delete-word))
+  ;; Tidy shadowed file names
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
 (use-package savehist
   :init
   (savehist-mode))
