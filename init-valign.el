c64ca818234eb7751fbeaa116f1332eef381e4a6
Sawyer Zheng <kmxsz@qq.com>
2026-07-14 22:36:00 +0800

    chore(valign): remove automatic valign hook

--- init-valign.el
+++ init-valign.el
@@ -1,6 +1,8 @@
 ;; -*- coding: utf-8; -*-
 (use-package valign
-  :hook ((org-mode markdown-mode md-ts-mode gfm-mode markdown-ts-mode) . valign-mode)
+  ;; notice: valign is very CPU comsuming
+  ;; :hook ((org-mode markdown-mode md-ts-mode gfm-mode markdown-ts-mode) . valign-mode)
+  :commands (valign-mode)
   :config
   (setq valign-fancy-bar t
         valign-max-table-size 40000)
